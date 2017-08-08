857726489422e28fba83c95248cac574d4f5c003
stanrogo <me@stanrogo.com>
2017-08-08 12:53:52 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Carla's Blog</title>
+    <meta name="author" content="name">
+    <meta name="description" content="description here">
+    <meta name="keywords" content="keywords,here">
+</head>
+<body>
+hello!
+
+</body>
+</html>
