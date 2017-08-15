60f04df0e356a5b93599dde3ad5301c14e747392
stanrogo <me@stanrogo.com>
2017-08-15 22:17:06 +0200

    Implemented the error page

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -19,81 +19,84 @@
     <!---------------- CSS ------------------>
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="/blog/public/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-    <link href="/blog/public/css/main.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-    <link href="/blog/public/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-    <link href="/blog/public/css/error.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="{{ URL::asset('css/materialize.css')}}" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="{{ URL::asset('css/app.css')}}" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="{{ URL::asset('css/error.css')}}" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
 
-<!----------------- Header ------------------>
+<div id="app" class="language-{{LaravelLocalization::getCurrentLocale()}}">
+    <!----------------- Header ------------------>
 
-<header id="site-header">
+    <header id="header">
 
-    <div class="container">
-        <nav class="nav-wrapper transparent z-depth-0">
-            <a href="#" class="brand-logo left">
-                <i class="material-icons black-text">dashboard</i>
-                <span class="light black-text">Carla Unterwegs</span>
-            </a>
+        <div class="container">
+            <nav class="nav-wrapper transparent z-depth-0">
+                <a href="#" class="brand-logo left">
+                    <i class="material-icons black-text">dashboard</i>
+                    <span class="light black-text">Carla Unterwegs</span>
+                </a>
 
-            <ul class="right hide-on-med-and-down">
-                <li class="btn-english"><a href="/en-GB/error/" class="light js-btn-language black-text" data-locale="english">English</a></li>
-                <li class="btn-german"><a href="/de/error/" class="light js-btn-language black-text" data-locale="german">Deutsch</a></li>
-            </ul>
-        </nav>
+                <ul class="right hide-on-med-and-down">
+                    <li class="btn-english"><a href="/en-GB/error/" class="light js-btn-language black-text" data-locale="english">English</a></li>
+                    <li class="btn-german"><a href="/de/error/" class="light js-btn-language black-text" data-locale="german">Deutsch</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <!----------------- Main Content ------------------>
+
+    <img src="{{ URL::asset('images/error.svg')}}" class="cartoon-image"/>
+
+    <div class="main-message js-main-message">
+        <h1 class="js-">
+            Oops. We couldn't find that place you're looking for!
+        </h1>
+        <a href="/">Click here</a> to go back to the homepage.
     </div>
-</header>
 
-<!----------------- Main Content ------------------>
+    <script type="text/javascript">
+        const locale = window.location.pathname.split('/')[1];
+        document.getElementsByTagName('body')[0].classList.add(`language-${locale}`);
+        const container = document.querySelector('.js-main-message');
+        let html = '';
 
-<img src="/blog/public/images/error.svg" class="cartoon-image"/>
+        if(locale === 'de'){
 
-<div class="main-message js-main-message">
-    <h1 class="js-">
-        Oops. We couldn't find that place you're looking for!
-    </h1>
-    <a href="/">Click here</a> to go back to the homepage.
-</div>
-
-<script type="text/javascript">
-    const locale = window.location.pathname.split('/')[1];
-    document.getElementsByTagName('body')[0].classList.add(`language-${locale}`);
-    const container = document.querySelector('.js-main-message');
-    let html = '';
-
-    if(locale === 'de'){
-
-        html = `
+            html = `
             <h1 class="error-call-out">Oops. Wir konnen das nicht finden!</h1>
             <a href="/de/">Kliken Sie hier</a> nach Hause zu gehen.
         `;
-    }
-    else{
+        }
+        else{
 
-        html = `
+            html = `
             <h1 class="error-call-out">Oops. We couldn't find that place you're looking for!</h1>
             <a href="/en-GB/">Click here</a> to go back to the homepage.
         `;
-    }
+        }
 
-    container.innerHTML = html;
-</script>
+        container.innerHTML = html;
+    </script>
 
-<!----------------- Page Footer ------------------>
+    <!----------------- Page Footer ------------------>
 
-<footer class="page-footer white">
-    <div class="container">
-        <div class="row">
-            <div class="col s12 page-footer-column">
-                <i class="material-icons black-text">dashboard</i>
-                <span class="black-text">
+    <footer class="page-footer white">
+        <div class="container">
+            <div class="row">
+                <div class="col s12 page-footer-column">
+                    <i class="material-icons black-text">dashboard</i>
+                    <span class="black-text">
                     Made by&nbsp;<a class="brown-text text-lighten-3" href="https://stanrogo.com" target="_blank">stanrogo</a>
                 </span>
+                </div>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
+
+</div>
+
 
 
 </body>
