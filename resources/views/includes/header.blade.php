78cec95a2248531b5fbfbf838e3b5de5d62078e0
stanrogo <me@stanrogo.com>
2017-08-16 18:15:52 +0200

    Header styling is proper

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,25 +4,41 @@
         <img src="{{ asset('images/background1.jpg', \App::environment() == 'production')}}">
     </div>
 
-    <div class="container container--header js-container-header">
+    <div class="container header--text-container flex-vertical-center js-container-header">
         <div class="row center">
-            <h5 class="header--text col s12 light white-text">Travels, experiences, thoughts and more...</h5>
+            <h5 class="header--text col s12 light white-text">
+                Travels, experiences, thoughts and more...
+            </h5>
         </div>
     </div>
 
-    <div class="container container--nav js-header--nav-bar--container">
+    <div class="container js-header--nav-bar--container">
         <div class="row nav-row">
             <div class="col s12">
-                <nav class="nav-wrapper transparent z-depth-0">
-                    <a href="#" class="brand-logo left">
-                        <i class="material-icons">dashboard</i>
-                        <span class="light">Carla Unterwegs</span>
-                    </a>
+                <nav class="transparent z-depth-0">
+                    <div class="nav-wrapper">
+                        <a href="/" class="brand-logo left">
+                            <i class="material-icons">dashboard</i>
+                            <span class="light">Carla Unterwegs</span>
+                        </a>
 
-                    <ul class="right hide-on-med-and-down">
-                        <li class="btn-english"><a href="/en-GB/" class="light js-btn-language" data-locale="english">English</a></li>
-                        <li class="btn-german"><a href="/de/" class="light js-btn-language" data-locale="german">Deutsch</a></li>
-                    </ul>
+                        <ul class="right hide-on-med-and-down">
+                            <li class="btn-english">
+                                <a href="/en-GB/"
+                                   class="light js-btn-language"
+                                >
+                                    English
+                                </a>
+                            </li>
+                            <li class="btn-german">
+                                <a href="/de/"
+                                   class="light js-btn-language"
+                                >
+                                    Deutsch
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
             </div>
         </div>
